48c0b58a354b1fa58fae100381b369ae16687901
Sandra Coburn <sandra_coburn@hotmail.com>
2020-02-27 11:18:51 -0800

    Backend testing, created directories and test files

--- api/server.js
+++ api/server.js
@@ -3,14 +3,19 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const cohortsRouter = require("../cohorts/");
+const studentsRouter = require("../students/router");
 
 const server = express();
 
 //middleware
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
 
 //routes
-server.get("/api/cohorts", cohortsRouter);
+server.use("/api/cohorts", cohortsRouter);
+server.use("/api/students", studentsRouter);
+
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
